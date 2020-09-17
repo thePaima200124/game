884f080de116785d1fd36f11c4df61e897dff0b8
otreblan <otreblain@gmail.com>
2020-09-16 23:04:10 -0500

    Add ship

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public string result;
+    public int points;
+    //public float size;
+    public Color color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        
+    }
+}
