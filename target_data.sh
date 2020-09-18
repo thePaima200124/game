38f677d4197b9fd7a2083e5ea47974cbf06695b2
otreblan <otreblain@gmail.com>
2020-09-18 13:50:57 -0500

    Add windows target

--- target_data.sh
+++ target_data.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
-export build_path_option='-buildLinux64Player "$PWD/game"'
-export build_target='Linux64'
+declare -A build_path_options
+declare -A build_targets
+
+build_path_option[linux-il2cpp]='-buildLinux64Player "$PWD/game"'
+build_path_option[windows]='buildWindows64Player "$PWD/game.exe"'
+
+build_targets[linux-il2cpp]='Linux64'
+build_targets[windows]='Win64'
+
+export build_path_option="${build_path_option[$1]}"
+export build_target="${build_targets[$1]}"
