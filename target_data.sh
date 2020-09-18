3912e4da84f71124e2dfdfa5308d421217ca3221
otreblan <otreblain@gmail.com>
2020-09-18 14:11:14 -0500

    Add build_path and build_option

--- target_data.sh
+++ target_data.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-declare -A build_path_options
+declare -A build_options
+declare -A build_paths
 declare -A build_targets
 
-build_path_option[linux-il2cpp]='-buildLinux64Player "./game"'
-build_path_option[windows]='-buildWindows64Player "./game.exe"'
+build_options[linux-il2cpp]='-buildLinux64Player'
+build_options[windows]='-buildWindows64Player'
+
+build_paths[linux-il2cpp]="$PWD/game"
+build_paths[windows]="$PWD/game.exe"
 
 build_targets[linux-il2cpp]='Linux64'
 build_targets[windows]='Win64'
 
-export build_path_option="${build_path_option[$1]}"
+export build_option="${build_options[$1]}"
+export build_path="${build_paths[$1]}"
 export build_target="${build_targets[$1]}"
