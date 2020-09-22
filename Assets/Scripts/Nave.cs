8e080107312448cdc6d76729ac52d83ead5a9111
mariochincha <55245265+mariochincha@users.noreply.github.com>
2020-09-22 01:26:07 -0500

    Add files via upload

--- Assets/Scripts/Nave.cs
+++ Assets/Scripts/Nave.cs
@@ -7,6 +7,8 @@ public class Nave : MonoBehaviour
     public float speed = 10.0f;
     public int Puntaje = 0;
     public int Salud = 3;
+    public static string[] inventario;
+    public static int expanzor = 0;
     private float vertical, horizontal;
     private Rigidbody2D rb;
     private Vector2 screenBounds;
@@ -15,12 +17,19 @@ public class Nave : MonoBehaviour
     {
         rb = this.GetComponent<Rigidbody2D>();
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z) );
+        Debug.Log(screenBounds);
+        inventario = new[] {"operacion 1","Operacion 2","Operacion 3"};
+        Debug.Log(inventario[expanzor]);
+
     }
+
 
 
     private void Update()
     {
         movimiento();
+        Seleccionar_operacion();
+
     }
 
     private void LateUpdate()
@@ -33,15 +42,28 @@ public class Nave : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         Vector2 dir = new Vector2(x, y);
-
-        if (rb.transform.position.x > screenBounds.x - 0.5)
+       
+        if (rb.transform.position.x > -(screenBounds.x + 0.5))
         {
-            rb.transform.position = new Vector3(screenBounds.x - 0.5f, rb.transform.position.y, rb.transform.position.z);
+            rb.transform.position = new Vector3(-(screenBounds.x + 0.5f), rb.transform.position.y, rb.transform.position.z);
+        }
+        if(rb.transform.position.x < (screenBounds.x + 0.5))
+        {
+            rb.transform.position = new Vector3((screenBounds.x + 0.5f), rb.transform.position.y, rb.transform.position.z);
+        }
+        if (rb.transform.position.y < (screenBounds.y + 0.5))
+        {
+            rb.transform.position = new Vector3(rb.transform.position.x,(screenBounds.y + 0.5f) , rb.transform.position.z);
+        }
+        if (rb.transform.position.y > -(screenBounds.y + 0.5))
+        {
+            rb.transform.position = new Vector3(rb.transform.position.x, -(screenBounds.y + 0.5f), rb.transform.position.z);
         }
         else
         {
-             rb.velocity = new Vector3(dir.x * speed, dir.y * speed);
+            rb.velocity = new Vector3(dir.x * speed * Time.deltaTime, dir.y * speed* Time.deltaTime);
         }
+        
     }
     public void Disparar()
     {
@@ -49,7 +71,31 @@ public class Nave : MonoBehaviour
     } 
     public void Seleccionar_operacion()
     {
-
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            expanzor++;
+           if(expanzor>inventario.Length-1)
+            {
+                expanzor = 0;
+            }
+            
+            Debug.Log(inventario[expanzor]);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            expanzor--;
+            if (expanzor < 0)
+            {
+                expanzor = inventario.Length - 1;
+            }
+            
+            Debug.Log(inventario[expanzor]);
+            
+            
+        }
+        
+        
+        
     } 
     public void Muerte()
     {
---
+++ Assets/Scripts/texto.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using TMPro;
+
+public class texto : MonoBehaviour
+{
+    public TextMeshProUGUI municion;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        municion.text = Nave.inventario[Nave.expanzor];
+    }
+}
